5b969744c55c02b49dd4345722a29ad2b691d468
Bianca Cat <git@biancashouse.com>
2024-02-05 09:36:03 +1100

    commit /example part 1

---
+++ example/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_content/flutter_content.dart';
+
+import 'firebase_options.dart';
+import 'home_page_mobile.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterContent.init(
+    namedStyles: {
+      "purple24": NamedTextStyle(color: Colors.purpleAccent, fontSize: 24),
+      "white30": NamedTextStyle(color: Colors.white, fontSize: 30),
+      "white36": NamedTextStyle(color: Colors.white, fontSize: 36),
+      "yellow72": NamedTextStyle(color: Colors.yellow, fontSize: 72),
+    },
+  );
+
+  // runApp(MaterialAppWrapper(
+  //   appName: 'compals',
+  //   initialValueJsonAssetPath: "startup-scripts/compals-config.json",
+  //   materialAppHomeF: HomePageProvider().getWebOrMobileHomePage,
+  //   materialAppThemeF: () => ThemeData(
+  //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  //     primaryColor: FUCHSIA_X,
+  //     primarySwatch: Colors.purple,
+  //     //scaffoldBackgroundColor: Colors.pink[30],
+  //     // useMaterial3: true,
+  //   ),
+  //   fbOptions: DefaultFirebaseOptions.currentPlatform,
+  // ));
+
+  runApp(MaterialSPA(
+    appName: 'flutter-content-app',
+    initialValueJsonAssetPath: "startup-scripts/compals-config.json",
+    webHome: const FlutterContentPage(panelName: 'home', snippetName: 'example-web-home', fromTemplate: 'scaffold-with-tabs',),
+    mobileHome: const HomePageMobile(),
+    materialAppThemeF: () => ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      primaryColor: FUCHSIA_X,
+      primarySwatch: Colors.purple,
+    ),
+    fbOptions: DefaultFirebaseOptions.currentPlatform,
+  ));
+}
